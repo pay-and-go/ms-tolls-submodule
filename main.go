0651b2b7d36ba0c5a6b9803de438e1ab35134f67
crabaqueropi <crabaqueropi@unal.edu.co>
2021-03-30 10:46:48 -0500

    delete and update ok
    deleteToll/{id} and updateToll/{id} working

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 )
@@ -37,6 +38,7 @@ func CreateTollEndpoint(response http.ResponseWriter, request *http.Request) {
     result, _ := collection.InsertOne(ctx, toll)
     json.NewEncoder(response).Encode(result)
 }
+
 func GetAllTollsEndpoint(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("content-type", "application/json")
 	var tolls []Toll
@@ -77,6 +79,56 @@ func GetTollEndpoint(response http.ResponseWriter, request *http.Request) {
 	json.NewEncoder(response).Encode(toll)
 }
 
+func DeleteTollEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	collection := client.Database("TollSubmodule").Collection("Tolls")
+	id, _ := strconv.Atoi(params["id"])
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	result, err := collection.DeleteOne(ctx, bson.M{"tollId": id})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(result)
+}
+
+func UpdateTollEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := strconv.Atoi(params["id"])
+	var toll Toll
+	_ = json.NewDecoder(request.Body).Decode(&toll)
+	collection := client.Database("TollSubmodule").Collection("Tolls")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+
+	//result, _ := collection.InsertOne(ctx, toll)
+	filter := bson.M{"tollId": bson.M{"$eq": id}}
+
+	update := bson.M{
+		"$set": bson.M{
+			"administrator": toll.Administrator,
+			"coor_lat": toll.CoorLat,
+			"coor_lng": toll.CoorLng,
+			"crane_phone_number": toll.CranePhoneNumber,
+			"name": toll.Name,
+			"price": toll.Price,
+			"sector": toll.Sector,
+			"territory": toll.Territory,
+			"toll_phone_number": toll.TollPhoneNumber,
+		},
+	}
+	result, err := collection.UpdateOne(ctx, filter, update)
+
+	if err != nil {
+		fmt.Println("UpdateOne() result ERROR:", err)
+		os.Exit(1)
+	}
+
+	json.NewEncoder(response).Encode(result)
+}
+
 func main() {
 	fmt.Println("Starting the application...")
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
@@ -89,5 +141,7 @@ func main() {
 	router.HandleFunc("/addToll", CreateTollEndpoint).Methods("POST")
 	router.HandleFunc("/getAllTolls", GetAllTollsEndpoint).Methods("GET")
 	router.HandleFunc("/getToll/{id}", GetTollEndpoint).Methods("GET")
+	router.HandleFunc("/deleteToll/{id}", DeleteTollEndpoint).Methods("DELETE")
+	router.HandleFunc("/updateToll/{id}", UpdateTollEndpoint).Methods("PUT")
 	http.ListenAndServe(":12345", router)
 }
